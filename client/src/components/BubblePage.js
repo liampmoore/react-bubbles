2091c839e7d789e9c0a4ec114619d13e137e5cd3
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-02-28 12:02:18 -0500

    Completed login and privateroute

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -3,11 +3,21 @@ import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  useEffect(() => {
+    const token = window.localStorage.getItem('token');
+    axiosWithAuth()
+      .get('/api/colors')
+      .then(res => {
+        setColorList(res.data)
+      })
+  }, [])
 
   return (
     <>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
+import {useHistory} from 'react-router-dom';
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const Login = () => {
+  const history = useHistory()
+
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    })
+  }
+
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    axiosWithAuth()
+      .post('/api/login', credentials)
+      .then(res => {
+        window.localStorage.setItem('token', res.data.payload)
+        history.push('/bubblepage')
+      })
+
+  }
+
+  
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
 
-      <form>
-        <input type='text' name='username' />
-        <input type='password' name='password' />
+      <form noValidate onSubmit={(e) => {handleSubmit(e)}}>
+        <input type='text' name='username' value={credentials.username} onChange={(e) => {handleChange(e)}} />
+        <input type='password' name='password' value={credentials.password} onChange={(e) => {handleChange(e)}} />
         <input type='submit' value='Login' />
       </form>
     </>
